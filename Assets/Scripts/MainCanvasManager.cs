1111b59aa8dc7f70d21abca497a83e1ab78b1d6f
Freddie <frederick2.lane@live.uwe.ac.uk>
2026-02-13 11:54:48 +0000

    Add inverse scanning

--- Assets/Scripts/MainCanvasManager.cs
+++ Assets/Scripts/MainCanvasManager.cs
@@ -16,6 +16,10 @@ public class MainCanvasManager : MonoBehaviour
     private int selectedButton = 1;
     private float moveTimer = 2f;
 
+    private float holdTimer = 0f;
+
+    public bool useAutomatic = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,6 +29,68 @@ public class MainCanvasManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (useAutomatic)
+        {
+            UpdateAutomatic();
+        }
+        else
+        {
+            UpdateInverse();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            useAutomatic = !useAutomatic;
+        }
+    }
+
+    private void UpdateInverse()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            holdTimer += Time.deltaTime;
+
+            if (holdTimer >= 0.75f)
+            {
+                holdTimer = 0f;
+                switch (selectedButton)
+                {
+                    case 1:
+                        selectedButton++;
+                        Button2.GetComponent<Button>().Select();
+                        break;
+                    case 2:
+                        selectedButton++;
+                        Button3.GetComponent<Button>().Select();
+                        break;
+                    case 3:
+                        selectedButton = 1;
+                        Button1.GetComponent<Button>().Select();
+                        break;
+                }
+            } 
+        }
+        else if (Input.GetKeyUp(KeyCode.Space)) 
+        {
+            holdTimer = 0f;
+
+            switch (selectedButton)
+            {
+                case 1:
+                    MenuText.GetComponent<TextMeshProUGUI>().text = "Option 1 pressed";
+                    break;
+                case 2:
+                    MenuText.GetComponent<TextMeshProUGUI>().text = "Option 2 pressed";
+                    break;
+                case 3:
+                    MenuText.GetComponent<TextMeshProUGUI>().text = "Option 3 pressed";
+                    break;
+            }
+        }
+    }
+
+    private void UpdateAutomatic()
     {
         moveTimer -= Time.deltaTime;
 
